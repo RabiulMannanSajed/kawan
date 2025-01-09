b685a001f3dbc7fa0b2272d6bcff6a706396ec64
RabiulMannanSajed <rabiulmannansajed@gmail.com>
2025-01-10 02:49:58 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,16 @@ import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import router from './app/routes';
-// const port = 3000;
 
 // parsers
 app.use(express.json());
 app.use(cors());
 
-// https://localhost:5000 //local
+// Handle preflight requests for all routes
+app.options('*', cors());
 //* https://kawan.onrender.com/api/v1
 
 app.use('/api/v1', router);
-
-// app.get('/', (req: Request, res: Response) => {
-//   const a = 7;
-//   res.send('this is running');
-// });
-
-// basic arrow fnc
-// const valueName = () =>{
-
-// }
 
 const getController = (req: Request, res: Response) => {
   res.status(200).json({
